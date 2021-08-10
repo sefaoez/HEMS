70734d7da5fe01fcdc5c84497fce0a156134f08c
Sefa Özmeral <Sefa.Oezmeral@p3-group.com>
2021-08-10 12:41:57 +0200

    Correcting comments

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ openwb = charging_station("192.168.4.1", 1, 11, 0, 0, 0, False, False, 0, 0, 0, 
 write_register_int(0, 112, 1, openwb.unit_id, openwb.ip)## Setting it to Sofort - Laden
 write_register_int(1, 10151, 1, openwb.unit_id, openwb.ip)
 write_register_int(12, 10152, 1, openwb.unit_id, openwb.ip)
-write_register_unint(0, 112, 1, openwb.unit_id, openwb.ip)## Setting it to Sofort - Laden
+write_register_unint(0, 112, 1, openwb.unit_id, openwb.ip) ##Setting it to Sofort - Laden
 write_register_unint(1, 10151, 1, openwb.unit_id, openwb.ip)
 write_register_unint(12, 10152, 1, openwb.unit_id, openwb.ip)
 
